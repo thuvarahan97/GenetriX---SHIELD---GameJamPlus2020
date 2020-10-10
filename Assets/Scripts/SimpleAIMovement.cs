cb590ac4da6de9b9c12d9ad0905fd8d3787a6956
Thuvarahan <thuvarahan97@gmail.com>
2020-10-10 21:37:52 +0530

    Character Motion from Start to End point

---
+++ Assets/Scripts/SimpleAIMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleAIMovement : MonoBehaviour
+{
+    [SerializeField] Transform entry;
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+    
+    void Awake()
+    {
+        Vector3 entryPosition = entry.transform.position;
+        transform.position = entryPosition;
+    }
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
