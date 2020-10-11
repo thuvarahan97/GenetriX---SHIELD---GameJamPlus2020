69e66db8c8f0dfef7b08c37fda47c9659dd4f6a8
Thuvarahan <thuvarahan97@gmail.com>
2020-10-11 20:50:45 +0530

    Spawner fixed. Target points trigger collision

--- Assets/Scripts/SimpleAIMovement.cs
+++ Assets/Scripts/SimpleAIMovement.cs
@@ -9,17 +9,44 @@ public class SimpleAIMovement : MonoBehaviour
     // [SerializeField] Transform target;
     NavMeshAgent agent;
     
-    public List<Transform> entryLocations;
-    public List<Transform> targetLocations;
+    List<Transform> entryLocations = new List<Transform>();
+    List<Transform> targetLocations = new List<Transform>();
 
     private int entryIndex;
     private int targetIndex;
     
     Vector3 entryPosition;
-    Vector3 targetPosition;
+    public Vector3 targetPosition;
+
+    // private Text happyLevel;
+    // private int score = 0;
     
     void Awake()
     {
+        entryLocations.Add(GameObject.Find("Pos_Start1").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start2").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start3").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start4").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start5").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start6").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start7").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start8").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start9").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start10").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start11").transform);
+        entryLocations.Add(GameObject.Find("Pos_Start12").transform);
+
+        targetLocations.Add(GameObject.Find("Pos_Bank").transform);
+        targetLocations.Add(GameObject.Find("Pos_Restaurant1").transform);
+        targetLocations.Add(GameObject.Find("Pos_Restaurant2").transform);
+        targetLocations.Add(GameObject.Find("Pos_MedicalCentre").transform);
+        targetLocations.Add(GameObject.Find("Pos_Mart1").transform);
+        targetLocations.Add(GameObject.Find("Pos_Mart2").transform);
+        targetLocations.Add(GameObject.Find("Pos_Mall").transform);
+        targetLocations.Add(GameObject.Find("Pos_Hospital").transform);
+        targetLocations.Add(GameObject.Find("Pos_QuarantineCentre").transform);
+
+
         entryIndex = Random.Range(0, entryLocations.Count);
         targetIndex = Random.Range(0, targetLocations.Count);
 
@@ -27,6 +54,9 @@ public class SimpleAIMovement : MonoBehaviour
         transform.position = entryPosition;
 
         targetPosition = targetLocations[targetIndex].position;
+
+        
+        // happyLevel = GameObject.Find("HappyLevel").GetComponent<Text> ();
     }
 
     void Start()
@@ -40,10 +70,16 @@ public class SimpleAIMovement : MonoBehaviour
     {
         agent.SetDestination(targetLocations[targetIndex].position);
 
-        if (transform.position == targetPosition) {
-            Debug.Log("dfdff");
-            //dfdddd
-            Destroy(this.gameObject);
-        }
+        // if (transform.position == targetPosition) {
+        //     Debug.Log("dfdff");
+        //     //dfdddd
+        //     Destroy(this.gameObject);
+        // }
+
+        // if (Vector3.Distance(targetPosition, this.transform.position) < 0.5) {
+        //     score = int.Parse(happyLevel.text);
+        //     happyLevel.text = (score + 1).ToString();
+        //     // gameObject.SetActive(false);
+        // }
     }
 }
---
+++ Assets/Scripts/TargetScore.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TargetScore : MonoBehaviour
+{
+
+    private Text happyLevel;
+    private int score = 0;
+
+    void Awake() {
+        
+        happyLevel = GameObject.Find("HappyLevel").GetComponent<Text> ();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D target) {
+        SimpleAIMovement playerScript = target.gameObject.GetComponent<SimpleAIMovement>();
+        Vector3 targetPosition = playerScript.targetPosition;
+        Vector3 thisPosition = transform.position;
+        if (targetPosition == thisPosition) {
+            score++;
+            Debug.Log(score.ToString());
+            // happyLevel.text = (score + 1).ToString();
+            // target.gameObject.SetActive(false);
+            Destroy(target.gameObject);
+        }
+    }
+}
