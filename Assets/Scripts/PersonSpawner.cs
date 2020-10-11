315a6c20aa91fe4b623e8dc9efc299171d9e2671
Thuvarahan <thuvarahan97@gmail.com>
2020-10-11 19:19:27 +0530

    person spawner with entry and target selections

--- Assets/Scripts/PersonSpawner.cs
+++ Assets/Scripts/PersonSpawner.cs
@@ -6,16 +6,8 @@ public class PersonSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] person;
-    
-    private BoxCollider2D col;
-
-    float x1, x2;
 
     void Awake() {
-        // col = GetComponent<BoxCollider2D> ();
-
-        // x1 = transform.position.x - col.bounds.size.x / 2f;
-        // x2 = transform.position.x + col.bounds.size.x / 2f;
     }
 
     void Start() {
@@ -28,10 +20,10 @@ public class PersonSpawner : MonoBehaviour
         Vector3 temp = transform.position;
         temp.x = -17.5f;
         temp.y = 9.51f;
-        temp.y = 0;
+        temp.z = 0;
 
         Instantiate (person[Random.Range(0, person.Length)], temp, Quaternion.identity);
         
-        StartCoroutine (SpawnPerson(Random.Range(0.00001f, 1.5f)));
+        StartCoroutine (SpawnPerson(Random.Range(1.0f, 5.0f)));
     }
 }
--- Assets/Scripts/SimpleAIMovement.cs
+++ Assets/Scripts/SimpleAIMovement.cs
@@ -5,15 +5,28 @@ using UnityEngine.AI;
 
 public class SimpleAIMovement : MonoBehaviour
 {
-    [SerializeField] Transform entry;
-    [SerializeField] Transform target;
+    // [SerializeField] Transform entry;
+    // [SerializeField] Transform target;
     NavMeshAgent agent;
     
+    public List<Transform> entryLocations;
+    public List<Transform> targetLocations;
+
+    private int entryIndex;
+    private int targetIndex;
+    
+    Vector3 entryPosition;
+    Vector3 targetPosition;
     
     void Awake()
     {
-        Vector3 entryPosition = entry.transform.position;
+        entryIndex = Random.Range(0, entryLocations.Count);
+        targetIndex = Random.Range(0, targetLocations.Count);
+
+        entryPosition = entryLocations[entryIndex].transform.position;
         transform.position = entryPosition;
+
+        targetPosition = targetLocations[targetIndex].position;
     }
 
     void Start()
@@ -25,6 +38,12 @@ public class SimpleAIMovement : MonoBehaviour
 
     void Update()
     {
-        agent.SetDestination(target.position);
+        agent.SetDestination(targetLocations[targetIndex].position);
+
+        if (transform.position == targetPosition) {
+            Debug.Log("dfdff");
+            //dfdddd
+            Destroy(this.gameObject);
+        }
     }
 }
